71e68ecc88f5545e5c842b0c65ba1d17c45b0df0
ald93 <alderein999@gmail.com>
2021-03-10 19:49:50 +0100

    musica y camara puestas
    objetos 3d importados y colocados con texturas, musica de fondo y script de camara hechos

---
+++ Jiménez_Luna_Examen/Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimientoCamara : MonoBehaviour
+{
+
+    [SerializeField] Transform boxPosition;
+    
+    [SerializeField] float smoothVelocity = 0.5F;
+
+    private Vector3 camVelocity = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 targetPosition = new Vector3(boxPosition.position.x, boxPosition.position.y + 2f, transform.position.z);
+
+        
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref, camVelocity, smoothVelocity);
+        
+    }
+}
